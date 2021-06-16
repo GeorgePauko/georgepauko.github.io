dff6f242f560edba6b08863952d487d356b0067a
GeorgePauko <85961311+GeorgePauko@users.noreply.github.com>
2021-06-15 19:32:06 -0700

    Add files via upload

---
+++ GeorgePauko-FactFinder/script.js
@@ -0,0 +1,20 @@
+
+function f1() {
+	document.querySelector('#b').innerHTML = "More human twins are being born now than ever before.";
+}
+
+function f2() {
+	document.querySelector('#b').innerHTML = "The first person convicted of speeding was going 8 mph.";
+}
+
+function f3() {
+	document.querySelector('#b').innerHTML = "The world wastes about 1 billion metric tons of food each year.";
+}
+
+function f4() {
+	document.querySelector('#b').innerHTML = "The severed head of a sea slug can grow a whole new body.";
+}
+
+function f5() {
+	document.querySelector('#b').innerHTML = "Hair and nails grow faster during pregnancy.";
+}
